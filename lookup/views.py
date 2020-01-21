c23bf0c1b3d5014f3cabd92a1f2819af0604a62c
Atanu Chowdhury <atanuc073@gmail.com>
2020-01-21 21:25:21 +0530

    pull api request through ulr and show it on page

--- lookup/views.py
+++ lookup/views.py
@@ -3,7 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html',{})
+    import json
+    import requests             # in order to use the url we need to import the json and requests.
+    # to externally use requests we need to install via command pip3 install requests
+    api_request=requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=90869D05-E035-49AC-B552-A6AC9462E868")
+    try:
+        api=json.loads(api_request.content)
+
+    except Exception as e:
+        api="Error..."
+# thats how we use external api in our app
+
+    return render(request,'home.html',{'api':api})
 
 
 def about(request):
